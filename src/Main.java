f347034a62507f6eb792a2e7cb0696016568ca13
DechyQuan <NissleyOJ22@gcc,.edu>
2023-02-27 13:49:09 -0500

    Updated to greet user

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        try(Scanner in = new Scanner(new File("numbers.txt"))) {
-            System.out.println(in.nextInt());
-        } catch (FileNotFoundException e) {
-            System.out.println("file not found");
-        } catch (Exception e){
-            System.out.println("yeaaaa");
-        } finally {
-            System.out.println("finally");
+        System.out.println("Hello MoleDuck");
+
+      //  try(Scanner in = new Scanner(new File("numbers.txt"))) {
+    //        System.out.println(in.nextInt());
+      //  } catch (FileNotFoundException e) {
+        //    System.out.println("file not found");
+      //  } catch (Exception e){
+      //      System.out.println("yeaaaa");
+       // } finally {
+        //    System.out.println("finally");
         }
     }
-}
