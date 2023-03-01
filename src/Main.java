370791de0667cc0c5b1da017a6412328cf725286
armourconstructive <121581928+armourconstructive@users.noreply.github.com>
2023-03-01 13:54:35 -0500

    Response message

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello MoleDuck");
+        System.out.println("Hello back DechyQuan");
 
       //  try(Scanner in = new Scanner(new File("numbers.txt"))) {
     //        System.out.println(in.nextInt());
